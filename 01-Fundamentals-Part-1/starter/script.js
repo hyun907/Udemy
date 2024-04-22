845500c7c5d824ab52613d55eabed500b4c2d44a
hyun907 <seunghyun020907@naver.com>
2024-04-22 16:53:27 +0900

    Add files

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+// let js = '프로그래밍 언어';
+// if(js === '프로그래밍 언어') alert('Hello World!');
+// console.log(40+8+23-10);
+
+console.log('Jonas');
+console.log(23);
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+age = 31;
+
+const birthYear = 2002;
+// birthYear = 1990; X 재할당 불가
+
+// const job; X 초기 값 선언해야 함
+
+const ageJonas = 2037
