f60e8f4617430cca7f6dab74ae33d988bc8c14f6
hyun907 <seunghyun020907@naver.com>
2024-04-24 17:53:17 +0900

    코딩 챌린지 #1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -15,35 +15,70 @@
 
 // const job; X 초기 값 선언해야 함
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2020;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2020;
+// console.log(ageJonas, ageSarah);
 
-// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
-// 2 ** 3 means 2 to the poser of 3 = 2 * 2 * 2
+// // console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// // 2 ** 3 means 2 to the poser of 3 = 2 * 2 * 2
 
-// const firstName = 'Jonas';
-// const lastName = 'Schmedtmann';
-// console.log(firstName + ' ' + lastName);
+// // const firstName = 'Jonas';
+// // const lastName = 'Schmedtmann';
+// // console.log(firstName + ' ' + lastName);
 
-// 템플릿 문자열 ``
-// typeof 연산자 -> 종류
+// // 템플릿 문자열 ``
+// // typeof 연산자 -> 종류
 
-// 연산자 종류
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x ++; // x = x +1
-x --;
-console.log(x);
+// // 연산자 종류
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x ++; // x = x +1
+// x --;
+// console.log(x);
 
-// 비교 연산자
-console.log(ageJonas > ageSarah);
-console.log(ageJonas < ageSarah);
-// >, <. >=, <=
-console.log(ageSarah >= 18);
+// // 비교 연산자
+// console.log(ageJonas > ageSarah);
+// console.log(ageJonas < ageSarah);
+// // >, <. >=, <=
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
+
+// // 연산자는 왼쪽에서 오른쪽으로
+// console.log(25 - 10 - 5);
+// console.log(`---------`);
+// let y, z;
+// y = z = 25 - 10 - 5; // y = z = 10, y = 10
+// console.log(y, z);
+
+// console.log(`averageAge`);
+// const averageAge = ageJonas + ageSarah / 2
+// console.log(ageJonas, ageSarah, averageAge);
+
+
+// coading challange #1
+
+// #Test Data 1
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// #Test Data 2
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// console.log(BMIMark, BMIJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn
+// console.log(markHigherBMI);
+
