8a3eea29379de2c484a4d10d9332dde6c268706f
hyun907 <seunghyun020907@naver.com>
2024-05-05 11:42:46 +0900

    함수식과 익명함수

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,3 +32,24 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+
+// 함수 선언문
+function calcAge1(birthYear) {
+    // const age = 2037 - birthYear;
+    // return age;
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// 익명함수
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+// 함수는 값이라 변수에 저장할 수 있다
+// 함수 선언은 호이스팅 프로세스 때문에 먼저 호출할 수 있다
+// 익명함수는 함수식 이전에 호출할 수 없다
