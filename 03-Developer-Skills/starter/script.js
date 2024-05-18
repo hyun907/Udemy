f26db8a9845b4a7e78da198b1fca213b48e623f7
hyun907 <seunghyun020907@naver.com>
2024-05-18 18:58:58 +0900

    Coding Challenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -29,23 +29,66 @@
 // console.log(measureKelvin());
 
 // Using a debugger
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
 
-  let max = temps[0];
-  let min = temps[0];
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
 
-    debugger;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+//     debugger;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// console.log(amplitudeBug);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// MY SOLUTION 😎 toSorted() 이용해서 정렬해보기
+// const printForecast = function (arr) {
+//     const sortedArr = arr.toSorted((a,b) => a-b);
+//     for(let i=0; i<sortedArr.length; i++){
+//         console.log(`${sortedArr[i]}ºC in ${i+1} days ...`)
+//     }
+// };
+// console.log('--- Test 1');
+// printForecast([17, 21, 23]);
+// console.log('--- Test 2');
+// printForecast([12, 5, -5, 0, 4]);
+
+// ANSWER ⭐️
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days... `;
   }
-  console.log(max, min);
-  return max - min;
+  console.log('...' + str);
 };
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-console.log(amplitudeBug);
+
+printForecast(data1);
+printForecast(data2);
