7ccc161554747361615978e473a3293d6a12679d
hyun907 <seunghyun020907@naver.com>
2024-05-21 00:34:03 +0900

    Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -9,3 +10,17 @@ document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
