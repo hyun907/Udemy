96f3901023358e7380bbbe8ab8b92afd581d3b30
hyun907 <seunghyun020907@naver.com>
2024-05-22 16:47:53 +0900

    Refactor: 코드 리팩토링

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -16,58 +16,74 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let hightscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //   console.log(guess, typeof guess);
 
   // input 값이 없을 때
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
-
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
     // 플레이어가 숫자를 맞췄을 때
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    displayMessage('🎉 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
 
-    if(score > hightscore) {
-        hightscore = score;
-        document.querySelector('.highscore').textContent = hightscore;
+    if (score > hightscore) {
+      hightscore = score;
+      document.querySelector('.highscore').textContent = hightscore;
     }
-
-    // 숫자가 높을 때
-  } else if (guess > secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = '📈 Too high!';
+    // 숫자를 틀렸을 때
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // 숫자가 낮을 때
-  } else if (guess < secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
+      displayMessage('💥 You lost the game');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
+// 숫가자 높을 때
+//   else if (guess > secretNumber) {
+//     if (score > 0) {
+//       document.querySelector('.message').textContent = '📈 Too high!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = '💥 You lost the game';
+//       document.querySelector('.score').textContent = 0;
+//     }
+
+//     // 숫자가 낮을 때
+//   } else if (guess < secretNumber) {
+//     if (score > 0) {
+//       document.querySelector('.message').textContent = '📉 Too low!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = '💥 You lost the game';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// });
 
 // 초기화
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+//   document.querySelector('.message').textContent = 'Start guessing...';
+displayMessage('Start guessing...')
   document.querySelector('.number').textContent = '?';
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
