78a89b89b7a6dd72e2b858405dd515828419bc06
hyun907 <seunghyun020907@naver.com>
2024-05-24 21:24:22 +0900

    Feat: 모달 hidden 클래스 추가 및 제거로 기능 구현

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,10 +2,45 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const brnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) 
-    console.log(btnsOpenModal[i].textContent);
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener(
+    'click',
+    openModal
+    // function () {
+    //     console.log('Button clicked');
+    //     modal.classList.remove('hidden');
+    //     overlay.classList.remove('hidden');
+    //   }
+  );
+
+btnCloseModal.addEventListener(
+  'click',
+  closeModal
+  // function () {
+  //   modal.classList.add('hidden');
+  //   overlay.classList.add('hidden');
+  // }
+);
+
+overlay.addEventListener(
+  'click',
+  closeModal
+  // function () {
+  //   modal.classList.add('hidden');
+  //   overlay.classList.add('hidden');
+  // }
+);
